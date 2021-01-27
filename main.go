2349096d5b61ac06cb5187c38f35982f825d5030
Andrew Holford <andrewholford@gmail.com>
2021-01-27 05:27:53 +0000

    add header and contact page

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>welcome to my site</h>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>welcome to my site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
+
 }
 
 func main() {
