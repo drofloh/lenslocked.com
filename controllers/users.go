3a07c79f017fb58e67b0763b8767e2984b3ca4a6
Andrew Holford <andrewholford@gmail.com>
2021-02-15 05:32:42 +0000

    parsing the form with gorilla schama

--- controllers/users.go
+++ controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/drofloh/lenslocked.com/views"
+	"github.com/gorilla/schema"
 )
 
 // NewUsers used to create a new users controller.
@@ -30,6 +31,12 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// SignupForm ...
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create is used to process the signup form when a user submits it.
 // Used to create a new user account.
 //
@@ -38,9 +45,16 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 	// r.PostForm = map[string][]string
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, "This is a temp response.")
+	// fmt.Fprintln(w, r.PostForm["email"])
+	// fmt.Fprintln(w, r.PostForm["password"])
+	// fmt.Fprintln(w, "This is a temp response.")
 
 }
