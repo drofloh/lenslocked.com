383c9b2b21200eee1249caa5115a3699aec33ac9
Andrew Holford <andrewholford@gmail.com>
2021-02-26 10:31:26 +0000

    added a login form

--- controllers/users.go
+++ controllers/users.go
@@ -13,15 +13,18 @@ import (
 // and should only be used during initial setup.
 func NewUsers(us *models.UserService) *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-		us:      us,
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+
+		us: us,
 	}
 }
 
 // Users ...
 type Users struct {
-	NewView *views.View
-	us      *models.UserService
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
 }
 
 // New used to render the form where a user can creaste a new user account.
@@ -60,4 +63,22 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintln(w, user)
+}
+
+// LoginForm ...
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Login is used to verify the provided email and password and then
+// log the user in if they are correct.
+//
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
