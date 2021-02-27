eb897bd73aaf27e96c741963bef12d551b329a28
Andrew Holford <andrewholford@gmail.com>
2021-02-27 14:23:28 +0000

    added signIn

--- controllers/users.go
+++ controllers/users.go
@@ -62,7 +62,8 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, user)
+	signIn(w, &user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 // LoginForm ...
@@ -95,12 +96,17 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	signIn(w, user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+// signIn is used to sign in a given user and set cookies.
+func signIn(w http.ResponseWriter, user *models.User) {
 	cookie := http.Cookie{
 		Name:  "email",
 		Value: user.Email,
 	}
 	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
 }
 
 // CookieTest is used to display cookies set on the current user.
