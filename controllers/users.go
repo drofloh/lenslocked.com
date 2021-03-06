a04822560ade2e2914b3bdb239348c8ca593850e
Andrew Holford <andrewholford@gmail.com>
2021-03-06 14:50:55 +0000

    rendered a dynamic alert

--- controllers/users.go
+++ controllers/users.go
@@ -32,7 +32,16 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
+	type Alert struct {
+		Level   string
+		Message string
+	}
+
+	a := Alert{
+		Level:   "success",
+		Message: "Successfully rendered a dynamic alert!",
+	}
+	if err := u.NewView.Render(w, a); err != nil {
 		panic(err)
 	}
 }
