91e5b5aa36166f502956e3d973b18e094a4b54f5
Andrew Holford <andrewholford@gmail.com>
2021-03-09 05:59:00 +0000

    started services types and implemented the GalleryService interface

--- models/galleries.go
+++ models/galleries.go
@@ -8,3 +8,17 @@ type Gallery struct {
 	UserID uint   `gorm:"not_null;index"`
 	Title  string `gorm:"not_null"`
 }
+
+// GalleryService ...
+type GalleryService interface {
+	GalleryDB
+}
+
+// GalleryDB ...
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
---
+++ models/services.go
@@ -0,0 +1,19 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+// NewServices ...
+func NewServices(connectionInfo string) (*Services, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &Services{}, nil
+}
+
+// Services ...
+type Services struct {
+	Gallery GalleryService
+	User    userService
+}
