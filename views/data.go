0d2fc30a372d955dae6cf067ab9ab83c30d139d3
Andrew Holford <andrewholford@gmail.com>
2021-03-09 05:25:46 +0000

    implemented better error handling for the users login action

--- views/data.go
+++ views/data.go
@@ -39,6 +39,14 @@ func (d *Data) SetAlert(err error) {
 	}
 }
 
+func (d *Data) AlertError(msg string) {
+	d.Alert = &Alert{
+		Level:   AlertLvlError,
+		Message: msg,
+	}
+
+}
+
 // PublicError ...
 type PublicError interface {
 	error
