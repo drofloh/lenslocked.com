f63b1d86e064f08945a9830dcb4ea33ac57a9953
Andrew Holford <andrewholford@gmail.com>
2021-03-15 05:36:47 +0000

    added the update action

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -93,8 +93,17 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	gallery.Title = form.Title
-	fmt.Fprintln(w, gallery)
-	// g.EditView.Render(w, vd)
+	err = g.gs.Update(gallery)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Gallery successfully updated!",
+	}
+	g.EditView.Render(w, vd)
 }
 
 // Create ...
