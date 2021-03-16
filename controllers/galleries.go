8bb27275b83d260baf319b4e929e211001e1d2c6
Andrew Holford <andrewholford@gmail.com>
2021-03-16 05:17:23 +0000

    added the delete action to galleries controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -106,6 +106,30 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 	g.EditView.Render(w, vd)
 }
 
+// Delete ...
+// POST /galleries/:id/delete
+func (g *Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	err = g.gs.Delete(gallery.ID)
+	if err != nil {
+		vd.SetAlert(err)
+		vd.Yield = gallery
+		g.EditView.Render(w, vd)
+		return
+	}
+	// TODO: redirect to index page
+	fmt.Fprintln(w, "successfully deleted!")
+}
+
 // Create ...
 // POST /galleries
 func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
